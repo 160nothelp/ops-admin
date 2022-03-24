38ea6a6c93edfd9c4dba3dac676bf41637165526
Your Name <you@example.com>
2022-03-24 16:23:18 +0800

    修改为兼容drf_simplejwt

---
+++ src/views/users/groups.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="group-list-container">
+        组列表
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/users/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="user-list-container">
+        {{userList}}
+    </div>
+</template>
+
+<script>
+import { getUserList } from '@/api/user'
+import { title } from '@/settings'
+export default {
+    data() {
+        return {
+            userList: []
+        }
+    },
+    created() {
+        this.fetchData()
+    },
+    methods: {
+        fetchData() {
+            getUserList().then(response => {
+                this.userList = response.results
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
